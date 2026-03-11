cc169881ee2557c615f25d6f6e307a5dc56c8a67
霸道总裁 <45778925+overbearingceofirst@users.noreply.github.com>
2026-03-11 14:13:13 +0800

    Create src/config/constants.js file with application constants

---
+++ src/config/constants.js
@@ -0,0 +1,11 @@
+// Application Constants
+
+const APP_NAME = "Intelligent Community Node";
+const VERSION = "1.0.0";
+const API_URL = "https://api.intelligentcommunity.com";
+
+module.exports = {
+    APP_NAME,
+    VERSION,
+    API_URL,
+};
